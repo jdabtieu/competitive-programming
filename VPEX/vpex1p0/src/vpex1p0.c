b8449c0e344c0c23a71c11edf524bf61874923c1
Jonathan Wu <jonathan.wu3@student.tdsb.on.ca>
2020-07-23 23:46:00 -0400

    Update problem

---
+++ VPEX/vpex1p0/src/vpex1p0.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* 
+ * Copy-pasting code is NOT cool! Please do not copy and paste my code as a submission to DMOJ.
+ * github.com/jdabtieu/competitive-programming
+ */
+int main() {
+    int x, y; scanf("%d%d", &x, &y);
+    printf("%d %d", x / y, x % y);
+    return 0;
+}
