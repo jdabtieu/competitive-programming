fa8897c3d45bb45ec5fea70d1d9730c7af865038
Jonathan Wu <jonathan.wu3@outlook.com>
2020-12-29 23:36:21 -0500

    Add problems

---
+++ CCC/ccc11s1/src/ccc11s1.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+public class ccc11s1 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer in;
+	
+	public static void main(String[] args) throws IOException {
+		int s = 0;
+		for (int lines = readInt(); lines > 0; lines--) {
+			String input = readLine();
+			for (int i = 0; i < input.length(); i++) {
+				if (input.charAt(i) == 's' || input.charAt(i) == 'S') s++;
+				else if (input.charAt(i) == 't' || input.charAt(i) == 'T') s--;
+			}
+		}
+        System.out.println(s >= 0 ? "French" : "English");
+	}
+	
+	static String next() throws IOException {
+		while (in == null || !in.hasMoreTokens())
+			in = new StringTokenizer(br.readLine());
+		return in.nextToken();
+	}
+
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readChar() throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine() throws IOException {
+		return br.readLine();
+	}
+}
---
+++ Misc DMOJ/bf3hard/src/bf3hard.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+/*
+ * Copy-pasting code is NOT cool! Please do not copy and paste my code as a submission to DMOJ.
+ * github.com/jdabtieu/competitive-programming
+ */
+
+#define llu unsigned long long
+using namespace std;
+
+const llu SMALL_PRIME_PRODUCT = 152125131763605LL;
+
+bool isProbablePrime(llu num);
+int getLowestSetBit(llu num);
+int bitLength(llu num);
+llu mul_mod(llu a, llu b, llu m);
+llu qpow(llu a, llu n, llu m);
+
+int main() {
+    int ceiling = 479909;
+    bool A[ceiling] = {0};
+    for (int i = 2; i < sqrt(ceiling); i++) if (!A[i]) {
+        for (int j = i * i; j < ceiling; j += i) A[j] = true;
+    }
+
+    srand(time(NULL));
+    llu num; scanf("%llu", &num);
+
+    if (num == 1) {
+        puts("2");
+        return 0;
+    }
+    if (num == 2) {
+        puts("2");
+        return 0;
+    }
+
+    if ((num & 1) && isProbablePrime(num)) {
+        bool flag = true;
+        for (int i = 2; i < ceiling; i++) if (!A[i] && num % i == 0) {
+            flag = false;
+            break;
+        }
+        if (flag) {
+            printf("%llu\n", num);
+            return 0;
+        }
+    }
+
+    llu result = num + 1;
+    if (result % 2 == 0) result++;
+
+    while (true) {
+        if (result >= 64) {
+            long r = result % SMALL_PRIME_PRODUCT;
+            if ((r % 3 == 0)  || (r % 5 == 0)  || (r % 7 == 0)  || (r % 11 == 0) ||
+                (r % 13 == 0) || (r % 17 == 0) || (r % 19 == 0) || (r % 23 == 0) ||
+                (r % 29 == 0) || (r % 31 == 0) || (r % 37 == 0) || (r % 41 == 0)) {
+                    result += 2;
+                    continue;
+            }
+        }
+
+        if (result < 8) {
+            printf("%llu\n", result);
+            return 0;
+        }
+
+        if (isProbablePrime(result)) {
+            bool flag = true;
+            for (int i = 2; i < ceiling; i++) if (!A[i] && result % i == 0) {
+                flag = false;
+                break;
+            }
+            if (flag) {
+                printf("%llu\n", result);
+                return 0;
+            }
+        }
+
+        result += 2;
+    }
+}
+
+bool isProbablePrime(llu num) {
+    llu thisMinusOne = num - 1;
+    int a = 1;
+    llu m = thisMinusOne >> a;
+
+    for (int i = 0; i < 100; i++) {
+        llu b;
+        do {
+            b = rand() % (llu) pow(2, bitLength(num));
+        } while (b <= 1 || b >= num);
+
+        llu z = qpow(b, m, num);
+        if (!(z == 1 || z == thisMinusOne)) return false;
+    }
+    return true;
+}
+
+int getLowestSetBit(llu num) {
+    if (num & 1) return 1;
+    int res = 1;
+    while (true) {
+        num >>= 1;
+        res <<= 1;
+        if (num & 1) return res;
+    }
+}
+
+int bitLength(llu num) {
+    return floor(log2(num)) + 1;
+}
+
+llu mul_mod(llu a, llu b, llu m) {
+    if (m == 0) return a * b;
+    llu r = 0;
+    while (a > 0) {
+        if (a & 1)
+            if((r += b) > m) r %=m;
+        a >>= 1;
+        if ((b <<= 1) > m) b %= m;
+    }
+    return r;
+}
+
+llu qpow(llu a, llu n, llu m) {
+    llu r = 1;
+    while (n > 0) {
+        if (n & 1)
+            r = mul_mod(r, a, m);
+        a = mul_mod(a, a, m);
+        n >>= 1;
+    }
+    return r;
+}
