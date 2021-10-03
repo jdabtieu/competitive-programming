aa9f279ceb5ff40dc3f408649271cd96a5760491
Jonathan Wu <jonathan.wu3@outlook.com>
2021-10-02 20:11:46 -0400

    fix disgusting code

--- ECOO/ecoo15r1p1/src/ecoo15r1p1.java
+++ ECOO/ecoo15r1p1/src/ecoo15r1p1.java
@@ -1,61 +1,54 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 public class ecoo15r1p1 {
-	/* 
-	 * Copy-pasting code is NOT cool! Please do not copy and paste my code as a submission to DMOJ.
-	 * github.com/jdabtieu/competitive-programming
-	 */
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer in;
-
-	public static void main(String[] args) throws IOException {
-		for (byte cases = 0; cases < 10; cases++) {
-			String text = null;
-			int time = 0;
-			HashMap<String, Integer> map = new HashMap<String, Integer>();
-			ArrayList<Integer> count = new ArrayList<Integer>();
-			count.add(0);
-			map.put("red", 0);
-			while (!((text = readLine()).equals("end of box"))) {
-				if (map.containsKey(text)) {
-					count.set(map.get(text), count.get(map.get(text)) + 1);
-				} else {
-					count.add(1);
-					map.put(text, count.size() - 1);
-				}
-			}
-			for (int i = 1; i < count.size(); i++) {
-				time += 13 * Math.ceil(count.get(i) / 7.0);
-			}
-			time += 16 * count.get(0);
-			System.out.println(time);
-		}
-	}
-
-	static String next() throws IOException {
-		while (in == null || !in.hasMoreTokens())
-			in = new StringTokenizer(br.readLine());
-		return in.nextToken();
-	}
-
-	static long readLong() throws IOException {
-		return Long.parseLong(next());
-	}
-
-	static int readInt() throws IOException {
-		return Integer.parseInt(next());
-	}
-
-	static double readDouble() throws IOException {
-		return Double.parseDouble(next());
-	}
-
-	static char readChar() throws IOException {
-		return next().charAt(0);
-	}
-
-	static String readLine() throws IOException {
-		return br.readLine();
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer in;
+    
+    public static void main(String[] args) throws IOException {
+        for (int cases = 10; cases > 0; cases--) {
+            Map<String, Integer> m = new HashMap<>();
+            while (true) {
+                String s = readLine();
+                if (s.equals("end of box")) {
+                    int t = 0;
+                    for (Map.Entry<String, Integer> entry : m.entrySet()) {
+                        if (entry.getKey().equals("red")) {
+                            t += 16 * entry.getValue();
+                        } else {
+                            t += 13 * Math.ceil(entry.getValue() / 7.0);
+                        }
+                    }
+                    System.out.println(t);
+                    break;
+                }
+                m.put(s, m.getOrDefault(s, 0) + 1);
+            }
+        }
+    }
+    
+    static String next() throws IOException {
+        while (in == null || !in.hasMoreTokens())
+            in = new StringTokenizer(br.readLine());
+        return in.nextToken();
+    }
+    
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+    
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+    
+    static char readChar() throws IOException {
+        return next().charAt(0);
+    }
+    
+    static String readLine() throws IOException {
+        return br.readLine();
+    }
 }
